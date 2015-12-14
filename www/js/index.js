e76ccb01a9d123ffe9562b5909978c35ba68ba1c
Daniel Patterson <dbp@dbpmail.net>
2015-12-13 20:45:59 -0500

    add some autocompletion and word banking to desc field

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,52 @@
+function getWords() {
+  try {
+    var words = JSON.parse(localStorage["words"]);
+  } catch (_) {
+    localStorage["words"] = JSON.stringify({});
+    words = {};
+  } finally {
+    return words;
+  }
+}
+
+function orderedWords(match) {
+  var words = getWords();
+  var arrs = Object.keys(words).filter(function(w) {
+    if (typeof match === "undefined") {
+      return true;
+    } else {
+      return  w.indexOf(match) !== -1;
+    }
+  }).map(function(w) {
+    return [w, words[w]];
+  });
+  return arrs.sort(function (a,b) {
+    if (a[1] > b[1]) {
+      return -1;
+    } else if (a[1] < b[1]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }).map(function (p) { return p[0]; });
+}
+
+function modWords(sentance, n) {
+  var words = getWords();
+  sentance.replace(".", "").replace(",","").split(" ").map(function(w) {
+    var word = w.toLowerCase();
+    if (typeof words[word] === "undefined") {
+      words[word] = n;
+    } else {
+      words[word] = words[word] + n;
+    }
+  });
+  localStorage["words"] = JSON.stringify(words);
+}
+
+var addWords = function(sentance) { modWords(sentance, 1); };
+var removeWords = function(sentance) { modWords(sentance, -1); };
+
 function getTags() {
   try {
     var tags = JSON.parse(localStorage["tags"]);
@@ -53,30 +102,9 @@ function tagSeen(tag) {
   m.redraw();
 }
 
-function flashTag(tag) {
-  tags(tags().map(function (t) {
-    if (t.id === tag) {
-      return { id: t.id, highlight: true };
-    } else {
-      return t;
-    }
-  }));
-  setTimeout(function () {
-    tags(tags().map(function (t) {
-      if (t.id === tag) {
-        return { id: t.id, highlight: false };
-      } else {
-        return t;
-      }
-    }));
-
-    m.redraw();
-  }, 2000);
-
-  m.redraw();
-}
-
 var editing = m.prop(null);
+
+var descing = m.prop(null);
 
 var edit = { desc: m.prop(null), percentage: m.prop(null), goodtill: m.prop(null)};
 
@@ -93,7 +121,10 @@ function edit_tag(id) {
 
 function save() {
   var ts = getTags();
+  var old = ts[editing()];
   ts[editing()] = {desc: edit.desc(), goodtill: moment(edit.goodtill()), percentage: edit.percentage()};
+  removeWords(old.desc);
+  addWords(edit.desc());
   localStorage["tags"] = JSON.stringify(ts);
   tags(loadTags(ts));
 }
@@ -114,12 +145,40 @@ var Tags = {
   },
   edit_view: function (ctrl) {
     if (typeof editing() === "string") {
+      if (descing() !== null) {
+        var desc_options = [m("span", ""),
+                            m(".field", orderedWords(descing()).map(function(w) {
+                              return m(".button", {onclick: function() {
+                                if (/ $/.test(edit.desc()) || edit.desc() === "") {
+                                  edit.desc(edit.desc() + w);
+                                } else {
+                                  edit.desc(edit.desc() + " " + w);
+                                }
+                                save();
+                              }}, w);
+                            }))];
+      } else {
+        var desc_options = [];
+      }
       return [m(".form",
                 [m(".row",[m("span", "desc"),
-                           m(".field", m("input", {onchange: function (e) {
-                             edit.desc(e.target.value);
-                             save();
-                           }, value: edit.desc()}))]),
+                           m(".field", m("input", {
+                             onclick: function(e) {
+                               e.stopPropagation();
+                             },
+                             onfocus: function () {
+                               descing("");
+                             },
+                             onkeyup: function (e) {
+                               var v = e.target.value;
+                               edit.desc(v);
+                               if (v[v.length - 1] !== ' ') {
+                                 var a = v.split(" ");
+                                 descing(a[a.length - 1]);
+                               }
+                               save();
+                             }, value: edit.desc()}))]),
+                 m(".row", desc_options),
                  m(".row",[m("span", "good for"),
                            m(".field",
                              [[0, 1, "it's bad"], [1, 3, "1 day"],[3, 5, "3 days"],[5, 7, "5 days"], [7, 14, "1 week"], [14, 21, "2 weeks"],
@@ -164,7 +223,9 @@ var Tags = {
   },
   view: function(ctrl) {
     if (editing() !== null) {
-      return m(".edit", Tags.edit_view(ctrl))
+      return m(".edit", {onclick: function () {
+        descing(false);
+      }}, Tags.edit_view(ctrl))
     } else {
       return m(".tags", tags().sort(compare_times).map(function (t) {
         var cl = ".tag";
